b94aed24a8c66fd8525c2587792ba57c6a69f0ba
HiskyYang <hisky.yang@shareworks.cn>
2019-01-09 16:47:37 +0800

    first commit

---
+++ src/main/java/com/hisky/springcloudprovider/SpringcloudProviderApplication.java
@@ -0,0 +1,16 @@
+package com.hisky.springcloudprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderApplication.class, args);
+    }
+
+}
+
